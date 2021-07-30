9eae9dd4edf436c5bee5e363564560a9c7f3319c
magi9123 <Klesimestra9>
2021-07-30 17:24:49 +0300

    user currency and history for rabbit

--- src/main/java/com/models/user/UserCurrency.java
+++ src/main/java/com/models/user/UserCurrency.java
@@ -1,10 +1,14 @@
 package com.models.user;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user_currency")
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "requestId", scope = UserCurrency.class)
 public class UserCurrency extends UserAbstract {
 
 }
--- src/main/java/com/models/user/UserHistoryCurrency.java
+++ src/main/java/com/models/user/UserHistoryCurrency.java
@@ -1,10 +1,14 @@
 package com.models.user;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user_history_currency")
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "requestId", scope = UserHistoryCurrency.class)
 public class UserHistoryCurrency extends UserAbstract {
 
     private int period;
