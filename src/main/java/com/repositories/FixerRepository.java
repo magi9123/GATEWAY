6601b313f9c159e0864bc2d68443f8bbf8896e4b
magi9123 <Klesimestra9>
2021-07-30 09:15:57 +0300

    repository for fixer.io api

---
+++ src/main/java/com/repositories/FixerRepository.java
@@ -0,0 +1,7 @@
+package com.repositories;
+
+import com.models.FixerModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FixerRepository extends CrudRepository<FixerModel, Long> {
+}
