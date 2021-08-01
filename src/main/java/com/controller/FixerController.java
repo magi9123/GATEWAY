08656330f803845abaed4c7a216d1f8b1b06eb11
magi9123 <Klesimestra9>
2021-08-01 12:58:13 +0300

    call rest fixer.io api every 12h

--- src/main/java/com/controller/FixerController.java
+++ src/main/java/com/controller/FixerController.java
@@ -3,6 +3,7 @@ package com.controller;
 import com.models.FixerModel;
 import com.services.FixerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class FixerController {
     private FixerService fixerService;
 
     @RequestMapping("/latest")
+    @Scheduled(fixedRate = 360000 * 12)
     public void save(List<FixerModel> fixerModels) {
         fixerService.save(fixerModels);
     }
